c42f2a13d22560ed59df046e8916d40ea61ce201
year end party <30817257+kimthuyet@users.noreply.github.com>
2024-11-06 15:42:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function confirmAttendance() {
     alert("Cảm ơn bạn đã xác nhận tham dự!");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const music = document.getElementById("background-music");
+    music.volume = 0.2; // Giảm âm lượng cho dễ chịu
+});
